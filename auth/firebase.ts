91d8a3350b96a3af2d93acd8e6677fc6de162364
HideBa <baba.papa1120.ba@gmail.com>
2021-02-23 09:00:18 +0900

    fix firebase config

--- auth/firebase.ts
+++ auth/firebase.ts
@@ -7,11 +7,16 @@ const config = {
   authDomain: process.env.FIREBASE_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   appId: process.env.FIREBASE_APP_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
-if (firebase.app.length === 0) {
-  firebase.initializeApp(config);
-}
+console.log(config);
+
+// if (firebase.app.length === 0) {
+//   firebase.initializeApp(config);
+// }
+export const app = firebase.initializeApp(config);
 
 const auth = firebase.auth();
 export { auth };
